ed3d69c71e326479253410783cbce15e54528233
chris <chris@asus>
2015-08-01 10:37:02 +0100

    encoding update

--- Helpers/aes.py
+++ Helpers/aes.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util import randpool
 import base64
-
+import urllib2
 """
 generate key:
 key = randpool.RandomPool(512).get_bytes(16) #byte array
@@ -29,11 +29,10 @@ class Cipher:
 
 	def encrypt(self, string):
 		enc = self.cipher.encrypt(string)
-		return base64.b64encode(enc)
+		return urllib2.quote(base64.urlsafe_b64encode(enc))
 
 	def decrypt(self, string):
-		enc = base64.b64decode(string)
+		enc = base64.urlsafe_b64decode(urllib2.unquote(string))
 		return str(self.cipher.decrypt(enc))
-
 
 
